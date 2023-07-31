46d2a9a34a2a0975446710e9ce3da60bd7836b67
Rahul Ghimire <ghimirerahul554@gmail.com>
2023-07-31 09:38:17 +0545

    Fix the Login page issue of flex

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import Logo from "../assets/logo.png";
 import "../index.css";
+import Form from "react-bootstrap/Form";
 
 const Login = () => {
   return (
     <section className="login-container">
       <div className="row m-0 p-0">
-        <div className="col-12 col-md-6 d-flex flex-column align-items-center justify-content-center">
+        <div className="col-12 col-md-6 d-flex flex-column align-items-center justify-content-center login-container--left">
           <div className="text-center">
             <p>Welcome to</p>
             <p>SPYRA Commerce</p>
           </div>
-          <form>
-            
-
-          </form>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+          </Form>
         </div>
-        <div className="col-12 col-md-6  d-flex flex-column align-items-center justify-content-center login_backgroud-image">
-          <div>
+        <div className="col-12 col-md-6  d-flex flex-column align-items-center justify-content-center login-container--right">
+          <div className="login_backgroud-image">
             <img src={Logo} alt="Logo" className="img-fluid" />
           </div>
         </div>
