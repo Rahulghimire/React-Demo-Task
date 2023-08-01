feb0acbcb7d3bb0085e26b8ad505884653598598
Rahul Ghimire <ghimirerahul554@gmail.com>
2023-08-01 12:20:16 +0545

    Update the Loading screen component

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,18 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
 import { LoadingScreen } from "./components/LoadingScreen";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-    <LoadingScreen/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoadingScreen />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,26 +1,35 @@
-import React,{useState,useEffect, CSSProperties} from 'react'
-import { ClimbingBoxLoader } from 'react-spinners';
+import React, { useState, useEffect, CSSProperties } from "react";
+import { HashLoader } from "react-spinners";
+import { useNavigate } from "react-router-dom";
 
 export const LoadingScreen = () => {
-    const [loading,setLoading]=useState(false);
-    useEffect(()=>{
-        setLoading(true);
-        setTimeout(()=>{
-            setLoading(false);
-       },7000);
-    },[])
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      navigate("/login");
+    }, 6000);
+  }, []);
 
   return (
-   <div className='loader'>
-   {loading?
-   <ClimbingBoxLoader
-   color={"#d4618a"}
-   loading={loading}
-   size={50}
-   aria-label="Loading Spinner"
-   data-testid="loader"
- />
-   :""}
-   </div>
-  )
-}
+    <div className="loader">
+      {loading ? (
+        <HashLoader
+          color={"#d4618a"}
+          loading={loading}
+          size={65}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
